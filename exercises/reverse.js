a2d81d48cb6c9f92fd9301f19617b97423b96853
el-besto <me@angelocisneros.com>
2014-11-18 23:19:18 -0800

    complete lab

---
+++ exercises/reverse.js
@@ -0,0 +1,8 @@
+//Write a program that will take a hardcoded string, and console log the reversed version of it. 
+//Use a for loop
+
+var inputString = "?emosewA si olegnA"
+
+for( i = 0; i < inputString.length; i++){
+  console.log(inputString.split('').reverse(i).join(''));
+};
