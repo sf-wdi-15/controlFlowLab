3025b0e7083c25cc447bfdf2026765350f017353
Samuel Cabral <samfcabral@gmail.com>
2014-11-23 17:08:55 -0800

    Solutions

--- filterLongWords.js
+++ filterLongWords.js
@@ -1,11 +1,22 @@
-var my_array = ["hat", "sock", "cat", "mark"];
+/*  Hardcode an array of words. 
+  Have a variable maxLength, 
+  push words that are less 
+  than the maxLength into a 
+  new array, and console.log 
+  that.
+*/
+
+var myArray = ["dog", "cat", "mouse", "horse", "eagle", "elephant"];
 var maxLength = 4;
-var new_array = [];
+var newArray = [];
+var currentWord;
 
-for (var i = 0; i < my_array.length; i++) {
-	if (my_array[i].length < maxLength) {
-		new_array.push(my_array[i]);
+for (var i = 0; i < myArray.length; i++) {
+	currentWord = myArray[i];
+
+	if (currentWord.length < maxLength) {
+		newArray.push(currentWord);
 	}
-
 }
-console.log(new_array);
+
+console.log("The new array is " + newArray);
--- grade.js
+++ grade.js
@@ -1,15 +1,33 @@
-switch(x) {
-	case (x >= 90):
+/* 
+Output the following letter
+  grade from a variable with 
+  with a test score. Display 
+  either "A", "B", "C", "D", 
+  or "F", for an score that
+  is an integer between 0 and 
+  100. Try and use a `switch`
+  statement.
+*/
+
+var score = 72;
+var grade = Math.floor(score / 10);
+
+switch(grade) {
+	case 10:
 		console.log("A");
 		break;
-	case (x >= 80):
-		consoloe.log("B");
+	case 9:
+		consoloe.log("A");
 		break;
-	case (x > 70):
+	case 8:
+		console.log("B");
+		break;
+	case 7:
 		console.log("C");
 		break;
-	case (x > 60):
+	case 6:
 		console.log("D");
+		break;
 	default:
 		console.log("F");
 	}
